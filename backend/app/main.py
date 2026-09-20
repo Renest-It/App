b9344d59d7bd8f4aaaa1f0af15e6d564f65a4986
hibaarmaghan1 <armaghanhiba@gmail.com>
2026-09-20 17:26:27 -0500

    Add GET /categories endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from app.routers import health
+from app.routers import categories, health
 
 app = FastAPI(title="ReNest API")
 
 app.include_router(health.router)
+app.include_router(categories.router)
